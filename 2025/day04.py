c8dcbd4ac09af3bae0be509905ea9dafc625b4d3
viv <vivian.claes@live.nl>
2026-07-26 19:19:37 +0200

    Clean code day 4

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,23 +1,28 @@
 def load_input(path: str) -> list[str]:
     with open(path) as f:
         return f.read().splitlines()
+
     
 def check_pick_up(grid):
-    length = len(grid)
-    padded_grid = [length * "."] + grid + [length * "."]
+    width = len(grid[0])
+    padded_grid = [width * "."] + grid + [width * "."]
     padded_grid = ['.' + row + '.' for row in padded_grid]
     
     counter = 0
     for i, row in enumerate(padded_grid):
         for j, value in enumerate(row):
-            if value == "@":
-                if all([i != 0, j != 0, i != len(padded_grid) - 1, j != len(padded_grid) - 1]):
-                    first_row_count = padded_grid[i-1][j-1:j+2].count('@')
-                    second_row_count = padded_grid[i][j-1:j+2].count('@') - 1
-                    third_row_count = padded_grid[i+1][j-1:j+2].count('@')
+            if value != "@":
+                continue
+            if i == 0 or j == 0 or i == len(padded_grid) - 1 or j == len(row) - 1:
+                continue
 
-                    if sum([first_row_count, second_row_count, third_row_count]) < 4:
-                        counter += 1
+            first_row_count = padded_grid[i-1][j-1:j+2].count('@')
+            second_row_count = padded_grid[i][j-1:j+2].count('@') - 1
+            third_row_count = padded_grid[i+1][j-1:j+2].count('@')
+
+            if first_row_count + second_row_count + third_row_count < 4:
+                counter += 1
+                
     return counter
     
     
@@ -32,6 +37,8 @@ if __name__ == "__main__":
 @.@@@.@@@@
 .@@@@@@@@.
 @.@.@@@.@.""".splitlines()
+    assert check_pick_up(test) == 13
+
     data = load_input("./data/day04.txt")
     result = check_pick_up(data)
     print(result)
