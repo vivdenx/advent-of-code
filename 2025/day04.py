0c243885b10d18068c392b7fc57735b48761ce0d
viv <vivian.claes@live.nl>
2026-07-27 20:55:24 +0200

    Working solution for day 4

--- 2025/day04.py
+++ 2025/day04.py
@@ -3,14 +3,20 @@ def load_input(path: str) -> list[str]:
         return f.read().splitlines()
 
     
-def check_pick_up(grid):
-    width = len(grid[0])
-    padded_grid = [width * "."] + grid + [width * "."]
-    padded_grid = ['.' + row + '.' for row in padded_grid]
+def check_pick_up(grid: list[str], start_width: int) -> int:
+    if start_width == len(grid[0]):
+        padded_grid = [start_width * "."] + grid + [start_width * "."]
+        padded_grid = ['.' + row + '.' for row in padded_grid]
+    else:
+        padded_grid = grid
+
+    memory = [list(row) for row in padded_grid]
     
     counter = 0
     for i, row in enumerate(padded_grid):
         for j, value in enumerate(row):
+            if value == 'x':
+                memory[i][j] = '.'
             if value != "@":
                 continue
             if i == 0 or j == 0 or i == len(padded_grid) - 1 or j == len(row) - 1:
@@ -22,8 +28,9 @@ def check_pick_up(grid):
 
             if first_row_count + second_row_count + third_row_count < 4:
                 counter += 1
-                
-    return counter
+                memory[i][j] = 'x'
+
+    return counter, memory
     
     
 if __name__ == "__main__":
@@ -37,8 +44,30 @@ if __name__ == "__main__":
 @.@@@.@@@@
 .@@@@@@@@.
 @.@.@@@.@.""".splitlines()
-    assert check_pick_up(test) == 13
+    assert check_pick_up(test, len(test[0]))[0] == 13
 
     data = load_input("./data/day04.txt")
-    result = check_pick_up(data)
-    print(result)
+    result = check_pick_up(data, len(data[0]))
+    print(result[0])
+
+    test_grid = test
+    test_counter = 1
+    start_width = len(test_grid[0]) 
+
+    full_counter = 0
+    while test_counter != 0:
+        test_counter, test_grid = check_pick_up(test_grid, start_width)
+        full_counter += test_counter
+    assert full_counter == 43
+
+    grid_copy = data
+    counter = 1
+    start_width = len(grid_copy[0])
+
+    all_rolls = 0
+    while counter != 0:
+        counter, grid_copy = check_pick_up(grid_copy, start_width)
+        all_rolls += counter
+    print(all_rolls)
+
+        
