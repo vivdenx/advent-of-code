051cb5ea0e85142e6c6241ab94c60479137e5746
viv <vivian.claes@live.nl>
2026-08-01 21:48:57 +0200

    Solve day 6 pt 2

--- 2025/day06.py
+++ 2025/day06.py
@@ -1,13 +1,24 @@
 from aocd import get_data
+from itertools import groupby
 
 def calculate_total(data):
     total = []
     res = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
     for row in res:
-        if row[-1] == "*":
-            total.append(eval("*".join(row[:-1])))
-        elif row[-1] == "+":
-            total.append(eval("+".join(row[:-1])))
+        total.append(eval(row[-1].join(row[:-1])))
+    return sum(total)
+
+
+def solve_part2(data):
+    total = []
+    operators = data[-1].split()
+    data = data[:-1]
+    res = ["".join([data[j][i] for j in range(len(data))]).strip() for i in range(len(data[0]))]
+    res = [list(g) for k, g in groupby(res, key=bool) if k]
+
+    for operator, values in zip(operators, res):
+        total.append(eval(operator.join(values)))
+
     return sum(total)
 
 
@@ -15,11 +26,14 @@ if __name__ == "__main__":
     test = """123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   +  """
-    test = [line.split() for line in test.splitlines() if line.strip()]
-    assert calculate_total(test) == 4277556, "Test failed"
+*   +   *   +  """.splitlines()
+    assert calculate_total([line.split() for line in test if line.strip()]) == 4277556, "Test (pt 1) failed"
+    assert solve_part2(test) == 3263827, "Test (pt 2) failed"
 
     data = get_data(year=2025, day=6)
-    data = [line.split() for line in data.splitlines() if line.strip()]
-    result = calculate_total(data)  
+    data = data.splitlines()
+    result = calculate_total([line.split() for line in data if line.strip()])  
+    print(result)
+
+    result = solve_part2(data)
     print(result)
