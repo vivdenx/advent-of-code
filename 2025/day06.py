413bba2a1f469763f00c047a62ba5062d6872644
viv <vivian.claes@live.nl>
2026-08-01 22:04:12 +0200

    Refactor code for day 6

--- 2025/day06.py
+++ 2025/day06.py
@@ -1,9 +1,14 @@
 from aocd import get_data
 from itertools import groupby
 
+
+def transpose(data):
+    return list(map(list, zip(*data)))
+
+
 def calculate_total(data):
     total = []
-    res = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
+    res = transpose(data)
     for row in res:
         total.append(eval(row[-1].join(row[:-1])))
     return sum(total)
@@ -11,8 +16,10 @@ def calculate_total(data):
 
 def solve_part2(data):
     total = []
+
     operators = data[-1].split()
     data = data[:-1]
+
     res = ["".join([data[j][i] for j in range(len(data))]).strip() for i in range(len(data[0]))]
     res = [list(g) for k, g in groupby(res, key=bool) if k]
 
@@ -27,13 +34,16 @@ if __name__ == "__main__":
  45 64  387 23 
   6 98  215 314
 *   +   *   +  """.splitlines()
+    
     assert calculate_total([line.split() for line in test if line.strip()]) == 4277556, "Test (pt 1) failed"
     assert solve_part2(test) == 3263827, "Test (pt 2) failed"
 
-    data = get_data(year=2025, day=6)
-    data = data.splitlines()
-    result = calculate_total([line.split() for line in data if line.strip()])  
-    print(result)
+    data = get_data(year=2025, day=6).splitlines()
 
-    result = solve_part2(data)
-    print(result)
+    # Part 1
+    part1 = calculate_total([line.split() for line in data if line.strip()])  
+    print(part1)
+
+    # Part 2    
+    part2 = solve_part2(data)
+    print(part2)
