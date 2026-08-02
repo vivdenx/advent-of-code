3433ffa6a5b55eeb5b1833a13eddb08c3cb445b7
viv <vivian.claes@live.nl>
2026-08-02 13:22:06 +0200

    Clean solution for day 7

--- 2025/day07.py
+++ 2025/day07.py
@@ -1,55 +1,41 @@
+from collections import defaultdict
 from aocd import get_data, submit
+
 
 def solve_part1(data):
     splits = 0
-    indices = set()
-    
+    positions = set()
+
     for line in data:
-        line = [c for c in line]
         if "S" in line:
-            indices.add(line.index("S"))
+            positions.add(line.index("S"))
 
-        copy = indices.copy()
-        for index in copy:
-            if line[index] == "^":
+        for position in positions.copy():
+            if line[position] == "^":
                 splits += 1
-                indices.remove(index)
-                indices.update([index - 1, index + 1])
+                positions.remove(position)
+                positions.update([position - 1, position + 1])
 
-        for i in indices:
-            line[i] = "|"
     return splits
         
 
 def solve_part2(data):
-    start_position = {data[0].index("S"): 1}
+    timelines = {data[0].index("S"): 1}
 
     for line in data[1:]: 
-        next = {}
-        line = [c for c in line]
+        next_timelines = defaultdict(int)
 
-        for position, count in start_position.items():
+        for position, count in timelines.items():
             if line[position] == "^":
-                if next.get(position-1):
-                    next[position-1] += count
-                else:
-                    next[position-1] = count
-                if next.get(position+1):
-                    next[position+1] += count
-                else:
-                    next[position+1] = count
+                next_timelines[position-1] += count
+                next_timelines[position+1] += count
             else:
-                if next.get(position):
-                    next[position] += count
-                else:
-                    next[position] = count
+                next_timelines[position] += count
 
-        start_position = next
+        timelines = next_timelines
 
-    return sum(v for v in start_position.values())
+    return sum(timelines.values())
    
-
-
 
 if __name__ == "__main__":
     test = """.......S.......
@@ -74,7 +60,7 @@ if __name__ == "__main__":
 
     data = get_data(year=2025, day=7).splitlines()
     solve1 = solve_part1(data)
-    #submit(solve1, part="a", day=7, year=2025)
+    submit(solve1, part="a", day=7, year=2025)
 
     solve2 = solve_part2(data)
     submit(solve2, part="b", day=7, year=2025)
