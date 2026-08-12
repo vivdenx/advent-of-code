6deb2a2857cf154ec3ccdea8554faeff7086569c
viv <vivian.claes@live.nl>
2026-08-12 13:25:45 +0200

    Add solution for part 2 day 8

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,8 +1,7 @@
 from aocd import get_data, submit
 import math
-import numpy as np
-from collections import defaultdict
 from itertools import combinations
+import numpy as np
 
 def calculate_distance(point_a, point_b):
     point_a = [int(x) for x in point_a.split(',')]
@@ -11,17 +10,26 @@ def calculate_distance(point_a, point_b):
     return math.dist(point_a, point_b)
 
 
-def solve1_v2(data, max_connections = 10):
+def find_edges(data, max_connections=None):
     n = len(data)
-
+        
     edges = []
     for i, j in combinations(range(n), 2):
         d = calculate_distance(data[i], data[j])
         edges.append((d, i, j))
 
-    edges = sorted(edges, key=lambda item: item[0])[:max_connections]
+    edges = sorted(edges, key=lambda item: item[0])
+
+    if max_connections:
+        return edges[:max_connections]
+    return edges
+
+
+def solve1(data, max_connections=None):
+    edges = find_edges(data, max_connections)
 
     # Union Find algorithm...
+    n = len(data)
     parent = list(range(n))
     size = [1] * n
 
@@ -50,9 +58,43 @@ def solve1_v2(data, max_connections = 10):
         root = find(i)
         comp_sizes[root] = comp_sizes.get(root, 0) + 1
 
-    top3 = sorted(comp_sizes.values(), reverse=True)[:3]
-    return math.prod(top3)
+    sort = sorted(comp_sizes.values(), reverse=True)
+    return math.prod(sort[:3])
+
+
+def solve2(data, max_connections=None):
+    edges = find_edges(data, max_connections)
     
+    # Union Find algorithm...
+    n = len(data)
+    parent = list(range(n))
+    size = [1] * n
+    
+    def find(x):
+        while parent[x] != x:
+            parent[x] = parent[parent[x]]  # path halving
+            x = parent[x]
+        return x
+    
+    def union(x, y):
+        rx, ry = find(x), find(y)
+        if rx == ry:
+            return
+        # Only merge if they belong to different groups
+        # Union by rank: attach smaller tree to larger tree
+        if size[rx] < size[ry]:
+            rx, ry = ry, rx
+        parent[ry] = rx
+        size[rx] += size[ry]
+    
+    for _, i, j in edges:
+        union(i, j)
+        if len({find(x) for x in range(n)}) == 1:
+            x1, x2 = int(data[i].split(',')[0]), int(data[j].split(',')[0])
+            return math.prod([x1, x2])
+        
+
+
 
 if __name__ == "__main__":
     test = """162,817,812
@@ -77,12 +119,12 @@ if __name__ == "__main__":
 425,690,689
 """.splitlines()
 
-    assert solve1_v2(test) == 40, "test (pt 2) failed"
-
-    test = [line.split(',') for line in test]
-    test = [[int(x) for x in line] for line in test]
-
+    assert solve1(test, 10) == 40, "test (pt 1) failed"
+    assert solve2(test) == 25272, "test (pt 2) failed"
+    
     data = get_data(year=2025, day=8).splitlines()
-    res = solve1_v2(data, 1000)
-    print(res)
+    res = solve1(data, 1000)
+
+    res = solve2(data)
     submit(res, year=2025, day=8)
+
