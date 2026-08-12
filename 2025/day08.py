ae4b44c01929f930169132ef6f1b6f97095f169a
viv <vivian.claes@live.nl>
2026-08-12 10:51:26 +0200

    Improve speed for day 8 solution

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,7 +1,8 @@
 from aocd import get_data, submit
 import math
 import numpy as np
-
+from collections import defaultdict
+from itertools import combinations
 
 def calculate_distance(point_a, point_b):
     point_a = [int(x) for x in point_a.split(',')]
@@ -10,25 +11,80 @@ def calculate_distance(point_a, point_b):
     return math.dist(point_a, point_b)
 
 
+def solve1_v2(data, max_connections = 10):
+    n = len(data)
+
+    edges = []
+    for i, j in combinations(range(n), 2):
+        d = calculate_distance(data[i], data[j])
+        edges.append((d, i, j))
+
+    edges = sorted(edges, key=lambda item: item[0])
+
+    groups = defaultdict()
+
+    for _, i, j in edges[:max_connections]:
+        key = [key for key,value in groups.items() if i in value or j in value]
+
+        if key:
+            if i in groups[key[0]]:
+                groups[key[0]] += [j]
+            else:
+                groups[key[0]] += [i]
+
+        else:
+            res = 0
+            if groups:
+                res = max(groups, key=groups.get) + 1
+            groups[res] = [i] + [j]
+
+    length_dict = {key: len(value) for key, value in groups.items()}
+    length_dict = dict(sorted(length_dict.items(), key=lambda item: item[1], reverse=True)[:3])
+
+    result = math.prod(length_dict.values())
+    return result
+
+
+
 def solve1(data, max_connections=10):
-    connections = []
-    while len(connections) < max_connections: 
+    connections = defaultdict()
+    connections_list = []
+
+    while len(connections_list) < max_connections: 
         minimum = float('inf')
-        closest = ''
-        for point_a in data:
-            for point_b in data:
-                if point_a != point_b and (point_a, point_b) not in connections and (point_b, point_a) not in connections:
-                    distance = calculate_distance(point_a, point_b)
+        closest = []
+        for i in range(len(data)):
+            for j in range(i+1, len(data)):
+                if (data[i], data[j]) not in connections_list and (data[j], data[i]) not in connections_list:
+                    distance = calculate_distance(data[i], data[j])
                     if distance < minimum:
                         minimum = distance
-                        closest = (point_a, point_b)
+                        closest = (data[i], data[j])
 
-        connections.append(closest)
+        connections_list.append(closest)
 
-    print(connections)
-        
-    pass
+        point_a, point_b = closest
+        key = [key for key, value in connections.items() if point_a in value or point_b in value]
 
+        if key:
+            if point_a in connections[key[0]]:
+                connections[key[0]] += [point_b]
+            else:
+                connections[key[0]] += [point_a]
+
+        else:
+            if connections:
+                res = max(connections, key=connections.get) + 1
+            else:
+                res = 0
+
+            connections[res] = [point_a] + [point_b]
+
+    length_dict = {key: len(value) for key, value in connections.items()}
+    length_dict = dict(sorted(length_dict.items(), key=lambda item: item[1], reverse=True)[:3])
+
+    result = math.prod(length_dict.values())
+    return result
 
 if __name__ == "__main__":
     test = """162,817,812
@@ -53,9 +109,11 @@ if __name__ == "__main__":
 425,690,689
 """.splitlines()
 
-    #solve1(test)
+    assert solve1_v2(test) == 40, "test (pt 2) failed"
+
     test = [line.split(',') for line in test]
     test = [[int(x) for x in line] for line in test]
-    solve_kdtree(test)
 
     data = get_data(year=2025, day=8).splitlines()
+    res = solve1_v2(data, 1000)
+    print(res)
