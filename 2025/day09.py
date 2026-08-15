86d40bc53b62d75ebc8bc28bd65d4753ca7aaf07
viv <vivian.claes@live.nl>
2026-08-15 11:13:37 +0200

    Day 9: find all green points (pt2)

--- 2025/day09.py
+++ 2025/day09.py
@@ -17,6 +17,48 @@ def solve1(data):
 
     return max_size
 
+def solve2(red_points):
+    max_size = 0
+    green_points = []
+
+    for i in range(len(red_points)):
+        xa, ya = [int(x) for x in red_points[i].split(',')]
+        try:
+            xb, yb = [int(x) for x in red_points[i+1].split(',')]
+        except:
+            xb, yb = [int(x) for x in red_points[0].split(',')]
+
+        if xa < xb:
+            for i in range(xa+1, xb):
+                green_points.append((i, ya))
+        if xa > xb:
+            for i in range(xb+1, xa):
+                green_points.append((i, ya))
+
+        if ya < yb:
+            for i in range(ya+1, yb):
+                green_points.append((xa, i))
+        if ya > yb:
+            for i in range(yb+1, ya):
+                green_points.append((xa, i))
+
+    green_points = sorted(green_points, key = lambda x: (x[1], x[0]))
+
+    previous_y = 0
+    start_x = 0
+    for x, y in green_points:
+        if y != previous_y:
+            previous_y = y
+            start_x = x
+        else:
+            print(start_x, previous_y)
+            for i in range(start_x+1, x):
+                if (i, y) not in green_points:
+                    green_points.append((i, y))     
+    
+    green_points = sorted(green_points, key = lambda x: (x[1], x[0]))
+    print(green_points)
+
 if __name__ == "__main__":
     test = """7,1
 11,1
@@ -28,7 +70,8 @@ if __name__ == "__main__":
 7,3""".splitlines()
 
     assert solve1(test) == 50, "test case (pt 1) failed"
+    solve2(test)
+
     data = get_data(year=2025, day=9).splitlines()
     res = solve1(data)
-
-    submit(res, year=2025, day=9, part='a')
+    #submit(res, year=2025, day=9, part='a')
